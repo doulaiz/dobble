8e236d633e07f475ecd23f2b5672e26aa3e08a51
Rafael Deleuze <rafael.deleuze@pilotfish.se>
2026-02-20 10:21:42 +0100

    Add empty components

---
+++ src/app/components/card-preview/card-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-preview',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-preview.component.html',
+  styleUrl: './card-preview.component.css'
+})
+export class CardPreviewComponent {
+
+}
